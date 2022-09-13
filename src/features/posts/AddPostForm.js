d25fe377d37b9f7773c012768debadc7b0f62564
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-14 04:21:50 +0530

    Implemented ability to select users and attach them to posts

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
 import { postAdded } from "./postsSlice";
 
 
@@ -8,23 +8,48 @@ const AddPostForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState('');
+
+  const users = useSelector(selectAllUsers);
 
   const onTitleChanged = e => setTitle(e.target.value);
   const onContentChanged = e => setContent(e.target.value);
+  const onAuthorChanged = e => setUserId(e.target.value);
+
   const onSavePostClicked = () =>{
-    if(title && content){
+    if(title && content && userId){
       dispatch(
-        postAdded(title,content)
+        postAdded(title,content, userId)
       );
       setTitle('');
       setContent("");
+      setUserId('');
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptionList = users.map(user => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
   return (
     <section>
       <h2>Add a New Post</h2>
-      <form>
-        <label htmlFor="postTitle">Post Title:</label>
+      <form>  
+        <label htmlFor="postAuthor">Author:</label>
+        <select 
+          id="postAuthor" 
+          name="postAuthor" 
+          value={userId} 
+          onChange={onAuthorChanged}
+        >
+          <option value=""></option>
+          {usersOptionList}
+        </select>
+        <label htmlFor="postTitle">Title:</label>
         <input 
           type="text" 
           id="postTitle" 
@@ -32,7 +57,7 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
-        <label htmlFor="postContent">Post Content:</label>
+        <label htmlFor="postContent">Content:</label>
         <textarea 
           id="postContent" 
           name="postContent" 
@@ -42,6 +67,7 @@ const AddPostForm = () => {
         <button 
           type="button"
           onClick={onSavePostClicked}
+          disabled={!canSave}
         >Save Post</button>
       </form>
     </section>
