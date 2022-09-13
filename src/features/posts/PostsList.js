b9c2dec3d94e4d69e9bb7e63b2f939a0708e09b8
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-14 04:52:57 +0530

    Implemented ability to display time of post submission

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postsSlice";
+import TimeStamp from "./TimeStamp";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -10,6 +11,7 @@ const PostsList = () => {
       <h3>{post.title}</h3>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeStamp timestamp={post.date} />
       </p>
       <p>{post.content.substring(0,100)}</p>
     </article>
