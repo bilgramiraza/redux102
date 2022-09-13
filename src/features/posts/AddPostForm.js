8ce6cecaf84638e532925140a9f30b58271c107f
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-14 03:43:56 +0530

    Added Ability to send Form Data to the global Store

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,17 +1,35 @@
+import { nanoid } from "@reduxjs/toolkit";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { postAdded } from "./postsSlice";
+
 
 const AddPostForm = () => {
+  const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState("");
 
   const onTitleChanged = e => setTitle(e.target.value);
   const onContentChanged = e => setContent(e.target.value);
-
+  const onSavePostClicked = () =>{
+    if(title && content){
+      dispatch(
+        postAdded({
+          id:nanoid(),
+          title,
+          content,
+        })
+      );
+      setTitle('');
+      setContent("");
+    }
+  };
   return (
     <section>
       <h2>Add a New Post</h2>
       <form>
-        <label htmlfor="postTitle">Post Title:</label>
+        <label htmlFor="postTitle">Post Title:</label>
         <input 
           type="text" 
           id="postTitle" 
@@ -19,14 +37,17 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
-        <label htmlfor="postContent">Post Content:</label>
+        <label htmlFor="postContent">Post Content:</label>
         <textarea 
           id="postContent" 
           name="postContent" 
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button">Save Post</button>
+        <button 
+          type="button"
+          onClick={onSavePostClicked}
+        >Save Post</button>
       </form>
     </section>
   )
