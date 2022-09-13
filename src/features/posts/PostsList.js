0b2ab8832919b65e3b00858111efc177a6e658ea
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-14 04:58:53 +0530

    Posts are now ordered in 'Latest First' Order

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,11 +2,12 @@ import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postsSlice";
 import TimeStamp from "./TimeStamp";
-
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map(post => (
+  const orderedPosts = posts.slice().sort((a, b)=>b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map(post => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p className="postCredit">
