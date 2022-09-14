99097c8f8682df697645475c9137450ee11f7196
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-15 01:24:41 +0530

    Implemented the Async Post fetch Process

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,30 +1,38 @@
-import { useSelector } from "react-redux";
-import PostAuthor from "./PostAuthor";
-import { selectAllPosts } from "./postsSlice";
-import ReactionButtonTray from "./ReactionButtonTray";
-import TimeStamp from "./TimeStamp";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { selectAllPosts, getPostsStatus ,getPostsError, fetchPosts } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+
 
 const PostsList = () => {
+  const dispatch = useDispatch();
+
   const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const postsError = useSelector(getPostsError);
 
-  const orderedPosts = posts.slice().sort((a, b)=>b.date.localeCompare(a.date));
-
-  const renderedPosts = orderedPosts.map(post => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeStamp timestamp={post.date} />
-      </p>
-      <p>{post.content.substring(0,100)}</p>
-      <ReactionButtonTray post={post} />
-    </article>
-  ));
+  useEffect(() => {
+    if(postsStatus === 'idle'){
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
+  
+  let content;
+  if(postsStatus === 'loading'){
+    content = <p>"Loading..."</p>;
+  }
+  else if(postsStatus === 'success'){
+    const orderedPosts = posts.slice().sort((a,b)=> b.date.localeCompare(a.date));
+    content = orderedPosts.map(post=><PostsExcerpt key={post.id} post={post} />);
+  }
+  else if(postsStatus === 'failure'){
+    content = <p>{postsError}</p>
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 }
