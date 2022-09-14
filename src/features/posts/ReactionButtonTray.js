fc18b8c8199493e68325d7caba050278120a3ec2
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-14 05:53:01 +0530

    Added Ability to add Reactions Tray to bottom of Posts

---
+++ src/features/posts/ReactionButtonTray.js
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const reactionEmoji = {
+  thumbsUp:'👍',
+  thumbsDown:'👎',
+  heart:'❤️',
+  fire:'🔥',
+  coffee:'☕',
+};
+
+const ReactionButtonTray = ({ post }) => {
+  const dispatch = useDispatch();
+
+  const reactionButtons = Object.entries(reactionEmoji).map(([name,emoji])=>{
+    return (
+      <button 
+        key={name} 
+        type="button"
+        className="reactionButton"
+        onClick={()=>
+          dispatch(reactionAdded({postId:post.id, reaction:name}))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+  return (
+    <div>{reactionButtons}</div>
+  )
+}
+
+export default ReactionButtonTray
