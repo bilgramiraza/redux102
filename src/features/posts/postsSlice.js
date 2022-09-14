7e6ae58b3f9d648cd9e8479563fb3ce725c51e7d
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-15 01:55:46 +0530

    Added Ability to make New Posts and send them to the API

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -15,6 +15,11 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async ()=>{
     return [...response.data];
 });
 
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost)=>{
+  const response = await axios.post(postsURL, initialPost);
+  return response.data;
+});
+
 const postsSlice = createSlice({
   name:'posts',
   initialState,
@@ -78,6 +83,19 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action)=>{
         state.status = 'failure';
         state.error  = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action)=>{
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp:0,
+          thumbsDown:0,
+          heart:0,
+          fire:0,
+          coffee:0,
+        };
+        console.log(action.payload);
+        state.posts.push(action.payload);
       })
   }
 });
