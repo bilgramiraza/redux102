ba517b9c6e1d271b17563fed366ba22eff3eeefd
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-17 11:46:40 +0530

    Added Links Each Post to Redirect to the Single Post Page

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -1,6 +1,7 @@
 import PostAuthor from "./PostAuthor";
 import TimeStamp from "./TimeStamp";
 import ReactionButtonTray from "./ReactionButtonTray";
+import { Link } from "react-router-dom";
 
 const PostsExcerpt = ({ post }) => {
   return (
@@ -10,7 +11,8 @@ const PostsExcerpt = ({ post }) => {
         <PostAuthor userId={post.userId} />
         <TimeStamp timestamp={post.date} />
       </p>
-      <p>{post.body.substring(0,100)}</p>
+      <p className="excerpt">{post.body.substring(0,75)}...</p>
+      <Link className="postCredit" to={`post/${post.id}`}>Read More</Link>
       <ReactionButtonTray post={post} />
     </article>
   )
