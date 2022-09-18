4aef0a057d6ba08c4e6451143c7db793742e28da
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-18 13:43:02 +0530

    Added Page To Display All Posts Made by A Specific User

---
+++ src/features/users/UserPage.js
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { selectAllPosts } from "../posts/postsSlice";
+import { selectUserById } from "./usersSlice";
+
+const UserPage = () => {
+  const { userId } = Number(useParams());
+  const user = useSelector(state => selectUserById(state, userId));
+
+  const userPosts = useSelector(state => {
+    const allPosts = selectAllPosts(state);
+    return allPosts.filter(post => post.userId === userId);
+  });
+
+  const userPostTitles = userPosts.map(post => (
+    <li key={post.id}>
+      <Link to={`/post/${post.id}`}>{post.title}</Link>
+    </li>
+  ));
+  return (
+    <section>
+      <h2>{user?.name}</h2>
+      <ol>
+        {userPostTitles}
+      </ol>
+    </section>
+  )
+}
+
+export default UserPage
