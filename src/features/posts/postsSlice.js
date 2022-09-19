0c3a5a5aeb9c4c89cf77e4f6c54cbf22b87896aa
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-19 14:26:13 +0530

    Added RTK Query Endpoint for Optimistic Reaction Button Tray tracking

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -99,6 +99,27 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         {type: 'Post', id: arg.id}
       ]
     }),
+    addReaction: builder.mutation({
+      query: ({ postId, reactions}) => ({
+        url: `posts/${postId}`,
+        method: 'PATCH',
+        body: { reactions },
+      }),
+      async onQueryStarted({ postId, reactions }, {dispatch, queryFulfilled}){
+        const patchResult = dispatch(
+          extendedApiSlice.util.updateQueryData('getPosts',undefined, draft => {
+            const post = draft.entities[postId];  //Immer Wrapped
+            if(post)  post.reactions = reactions;
+          })
+        );
+        try {
+          await queryFulfilled;
+        }
+        catch{
+          patchResult.undo();
+        }
+      },
+    }),
   })
 });
 
@@ -108,6 +129,7 @@ export const {
   useAddNewPostMutation,
   useUpdatePostMutation,
   useDeletePostMutation,
+  useAddReactionMutation,
 } = extendedApiSlice;
 
 //Returns Query Result Object
