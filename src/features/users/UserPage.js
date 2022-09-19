1396e8746fe454450722b48775c95b95936aee8d
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-19 13:15:21 +0530

    Modified UserPage to work with the RTK Query Selectors and Normalized Data

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,24 +1,41 @@
 import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { selectPostsByUser } from "../posts/postsSlice";
+import { useGetPostsByUserIdQuery } from "../posts/postsSlice";
 import { selectUserById } from "./usersSlice";
 
 const UserPage = () => {
   const { userId } = useParams();
   const user = useSelector(state => selectUserById(state, Number(userId)));
 
-  const userPosts = useSelector(state => selectPostsByUser(state,Number(userId)));
+  const {
+    data: userPosts,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostsByUserIdQuery(userId);
 
-  const userPostTitles = userPosts.map(post => (
-    <li key={post.id}>
-      <Link to={`/post/${post.id}`}>{post.title}</Link>
-    </li>
-  ));
+  let content;
+  if(isLoading){
+    content = <p>Loading...</p>;
+  }
+  else if(isSuccess){
+    const { ids, entities } = userPosts;
+    content = ids.map(id => (
+      <li key={id}>
+        <Link to={`/post/${id}`}>{entities[id].title}</Link>
+      </li>
+    ));
+  }
+  else if(isError){
+    content = <p>{error}</p>;
+  }
+
   return (
     <section>
       <h2>{user?.name}</h2>
       <ol>
-        {userPostTitles}
+        {content}
       </ol>
     </section>
   )
