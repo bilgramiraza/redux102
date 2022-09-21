b4a57aedb25686d50a578348e2d31faa9f3c8583
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-21 14:32:35 +0530

    Extended apiSlice and setup RTK Query Endpoints

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -5,6 +5,21 @@ const usersAdapter = createEntityAdapter();
 
 const initialState = usersAdapter.getInitialState();
 
-export const usersApiSlice = apiSlice.injectEndpoints(); 
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    getUsers: builder.query({
+      query:() => '/users',
+      transformResponse: responseData => {
+        return usersAdapter.setAll(initialState, responseData);
+      },
+      providesTags: (result, error, arg) => [
+        {type:'User', id:'LIST' },
+        ...result.ids.map(id => ({type:'User', id }))
+      ],
+    }),
+  }),
+}); 
 
-export const {} = usersApiSlice;
+export const {
+  useGetUsersQuery,
+} = usersApiSlice;
